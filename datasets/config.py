7c5541b48a77a2552079b1deff8bfcc580088228
fouada <fouad.azem@gmail.com>
2025-03-10 21:44:13 +0200

    update path and some fixes for the path

--- datasets/config.py
+++ datasets/config.py
@@ -1,2 +1,14 @@
-# jonathan PC + laptop
-dataset_path =  "C:\\Users\\Jonathan\\.pytorch-datasets"
+import os
+
+# Get the absolute path to the directory where config.py is located
+config_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Assume that the project root is the parent directory of the 'datasets' folder
+project_root = os.path.dirname(config_dir)
+
+# Now create dataset_path relative to project root
+dataset_path = os.path.join(project_root, "datasets")
+
+config = {
+    "dataset_path": dataset_path
+}
